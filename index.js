286953fb606d10efb4965e27205048d55e859259
Mark Ehler <ehlerm42@gmail.com>
2018-08-28 17:15:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,8 @@
 let recipes = []
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, [key], value)
-
+  return Object.assign({}, object, {[key]: value});
 }
-
 
 //it does not modifythe original object, but rather returns a clone with the new data
 // returns an object with the orignal key value pairs and the new key value pair:
